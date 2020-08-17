c1d28a386ed616cf2a2c761db199569d57aeac57
ronakagarwal3434 <60756098+ronakagarwal3434@users.noreply.github.com>
2020-08-17 17:24:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+var selected = 0;
+
+var roleSelected = [];
+
+// This function is for hiding the list which does not have the selected role.
+function hideListsWithoutRole(role){
+  
+  // Traversing all the list and looking for text inside the <li>.
+  for(i=1;i<=10;i++){
+    var listNo = i.toString();
+    var className =  ".list-" + listNo;
+    var listText = $(className + " li").text();
+    
+    // Hiding the list which does not have the particular "role".
+    if(listText.indexOf(role) == -1){
+      $(className).hide();
+    } 
+    else{
+      
+    }
+  }
+}
+
+// This function is for unhiding the list which has the "role".
+function unhideListsWithRole(){
+    for(i=0;i<roleSelected.length;i++){
+      for (j = 1; j <= 10; j++) {
+        var listNo = j.toString();
+        var className = ".list-" + listNo;
+        var listText = $(className + " li").text();
+
+        // Hiding the list which does not have the particular "role".
+        if (listText.indexOf(roleSelected[i]) == -1){
+          $(className).hide();
+        }
+        else {
+          $(className).show();
+        }
+      }
+    }
+  }
+
+// To add the selected role on the top box.
+$("li").click(function(){
+
+  // For the first role selection we have to create the new box on the top and then add the selected role.
+  if(selected === 0){
+    $(".upper-div").addClass("role-selected");
+
+    // Adding the selecetd role to above box.
+    var role = $(this).text();
+    $(".upper-div ul").append("<li>" + role + "<span>X</span></li>");
+    $(".upper-div span").addClass("X");
+
+    // To make note of number of all the different types of role got selected.
+    selected++;
+
+    // To make note of all the different types of role got selected.
+    roleSelected.push(role);
+
+    // Hiding the lists which does not contain the selected role.
+    hideListsWithoutRole(role);
+  }
+  else{
+    // Adding the selecetd role to above box.
+    var role = $(this).text();
+    if(!roleSelected.includes(role)){
+    $(".upper-div ul").append("<li>" + role + "<span>X</span></li>");
+    $(".upper-div span").addClass("X");
+
+    // To make note of number of all the different types of role got selected.
+    selected++;
+
+    // To make note of all the different types of role got selected.
+    roleSelected.push(role);
+
+    // Hiding the lists which does not contain the selected role.
+      hideListsWithoutRole(role);
+
+    }
+  }
+});
+
+// To deselect the selected role by clicking on "X".
+$(document).on("click",".X",function(){
+
+  // Removing the element from roleSelected Array.
+  var roleWithX = $(event.path[1]).text(); 
+  var temp = roleWithX.substring(0, roleWithX.length - 1);
+  var index = roleSelected.indexOf(temp);
+  roleSelected[index] = roleSelected[roleSelected.length-1];
+  roleSelected[roleSelected.length - 1] = temp;
+  roleSelected.pop();
+
+  // var roleWithX = $(event.path[1]).text();
+  // var role = roleWithX.substring(0,roleWithX.length-1);
+  // var indexOfRoleInRoleSelected =  roleSelected.indexOf(role);
+  // delete roleSelected[indexOfRoleInRoleSelected];  
+
+  // Unhiding.
+  unhideListsWithRole();
+
+  // Removing the selected role from above box.
+  $(this).remove();
+  $(event.path[1]).remove();
+
+});
